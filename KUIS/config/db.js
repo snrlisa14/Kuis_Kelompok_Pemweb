5f212bacf142a9c954a34fab6c2d286e3da612f7
snrlisa14 <stnurkholizah714@gmail.com>
2024-10-24 23:41:22 +0700

    Tugas KUIS Kelompok Pemweb

---
+++ KUIS/config/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'biota_laut'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL connected');
+});
+
+module.exports = db;
